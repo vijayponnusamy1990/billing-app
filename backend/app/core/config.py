2571d74d342c0ce3615e994895bed4b53c08688b
Vijaykumar P <84321975+vijayponnusamy1990@users.noreply.github.com>
2026-01-25 22:25:04 +0530

    Use local DB

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,6 @@
+from typing import Union
 from pydantic import AnyUrl
-from pydantic_settings import BaseSettings  # NEW
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
@@ -7,7 +8,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api"
     SECRET_KEY: str = "CHANGE_ME_SUPER_SECRET"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
-    SQLALCHEMY_DATABASE_URI: AnyUrl = "postgresql://postgres:postgres@db:5432/billing"
+    SQLALCHEMY_DATABASE_URI: Union[AnyUrl, str] = "sqlite:///./billing.db"
 
     class Config:
         env_file = ".env"
